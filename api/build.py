dcdae92907c0cfe8e4f295553e48907d4520b783
Danilo Gripa <danilogripa@gmail.com>
2022-07-08 07:20:16 -0300

    removing IP and UA
    adding agency ID
    
    fixing source ID

--- api/build.py
+++ api/build.py
@@ -25,9 +25,12 @@ def build(af_link: str, tg_link: str, key_link: str, os: str, agency: str) -> st
     # adding app_id
     final_query["app_id"] = tg_query["app_id"][0]
 
+    # ADDING AGENCY ID
+    final_query["agency_id"] = tg_query["partner_id"][0]
+
     # adding partner_id
     if (agency == "yes"):
-        final_query["source_id"] = tg_query["partner_id"][0]
+        final_query["source_id"] = af_query["pid"][0]
     else:
         final_query["partner_id"] = tg_query["partner_id"][0]
 
@@ -49,15 +52,16 @@ def build(af_link: str, tg_link: str, key_link: str, os: str, agency: str) -> st
             final_query[x_param] = af_query[param][0]
 
 
+    #only for imps
     # adding UA
-    if ("x_af_ua" in final_query):
-        final_query["user_agent"] = final_query["x_af_ua"]
-        del final_query["x_af_ua"]
+    #if ("x_af_ua" in final_query):
+    #    final_query["user_agent"] = final_query["x_af_ua"]
+    #    del final_query["x_af_ua"]
 
     # adding IP
-    if ("x_af_ip" in final_query):
-        final_query["ip_address"] = final_query["x_af_ip"]
-        del final_query["x_af_ip"]
+    #if ("x_af_ip" in final_query):
+    #    final_query["ip_address"] = final_query["x_af_ip"]
+    #    del final_query["x_af_ip"]
 
 
     # removing aaid from IOS links (if exists)
