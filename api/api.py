3b68ba7cd76a1ec6c2c6609aaf3949d2ddf9fa8e
Danilo Gripa <danilogripa@gmail.com>
2022-07-13 11:38:37 -0300

    refactoring api file

--- api/api.py
+++ api/api.py
@@ -1,16 +1,18 @@
-import urllib.parse, flask, build
-
+import build
+import flask
 from flask import request
-from urllib.parse import urlparse
+
 from build import build
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route('/convert', methods=['POST'])
-def api_test(finalQuery=None):
+def api_test():
     request_data = request.get_json()
-    return build(request_data['af'], request_data['tg'], request_data['key'], request_data['os'], request_data['ag'], request_data['type'])
+    return build(request_data['af'], request_data['tg'], request_data['key'], request_data['os'], request_data['ag'],
+                 request_data['type'])
+
 
 app.run(host="0.0.0.0")
-
