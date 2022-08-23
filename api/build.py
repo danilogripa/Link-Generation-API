e3a15a99ec7e0fb975ddbe9ef158b4032076a264
Danilo Gripa <danilogripa@gmail.com>
2022-08-23 10:31:38 -0300

    preparing for next updates

--- api/build.py
+++ api/build.py
@@ -3,11 +3,10 @@ import urllib.parse
 from urllib.parse import urlparse
 
 
-def build(red: str, af_link: str, tg_link: str, key_link: str, os: str, agency: str, type: str) -> str:
-
+def build(red: None, af_link: str, tg_link: str, key_link: str, os: str, agency: str, typeT: str) -> str:
     # Get AppsFlyer Link and break into parameters
-    af_Link = urlparse(af_link)
-    af_query = urllib.parse.parse_qs(af_Link.query)
+    af_link = urlparse(af_link)
+    af_query = urllib.parse.parse_qs(af_link.query)
 
     # Get TrafficGuard Link and break into parameters
     tg_link = urlparse(tg_link)
@@ -20,16 +19,10 @@ def build(red: str, af_link: str, tg_link: str, key_link: str, os: str, agency: 
     #########################################
     #########################################
 
-    final_query = {}
-
-    # adding app_id
-    final_query["app_id"] = tg_query["app_id"][0]
-
-    # ADDING AGENCY ID
-    final_query["agency_id"] = tg_query["partner_id"][0]
+    final_query = {"app_id": tg_query["app_id"][0], "agency_id": tg_query["partner_id"][0]}
 
     # adding partner_id
-    if (agency == "yes"):
+    if agency == "yes":
         final_query["source_id"] = af_query["pid"][0]
     else:
         final_query["partner_id"] = tg_query["partner_id"][0]
@@ -47,48 +40,70 @@ def build(red: str, af_link: str, tg_link: str, key_link: str, os: str, agency: 
 
     # adding what is missing with x_ parameters
     for param in af_query:
-        if (param not in key_query):
+        if param not in key_query:
             x_param = "x_" + param
             final_query[x_param] = af_query[param][0]
 
-
-    #only for imps
+    # only for imps
     # adding UA
-    if (type == "imp" and "x_af_ip" in final_query and "x_af_ua" in final_query):
-        final_query["user_agent"] = final_query["x_af_ua"]
-        final_query["ip_address"] = final_query["x_af_ip"]
-        del final_query["x_af_ip"]
-        del final_query["x_af_ua"]
+    if typeT == "imp":
+        if "user_agent" not in final_query and "ip_address" not in final_query:
+            final_query["user_agent"] = "#USER_AGENT_MACRO#"
+            final_query["ip_address"] = "#IP_ADDRESS_MACRO#"
+
+        if "x_af_ip" in final_query and "x_af_ua" in final_query:
+            final_query["user_agent"] = final_query["x_af_ua"]
+            final_query["ip_address"] = final_query["x_af_ip"]
+            del final_query["x_af_ip"]
+            del final_query["x_af_ua"]
+
+        if "token" not in final_query and "token" in tg_query:
+            final_query["token"] = tg_query["token"][0]
 
     # Checking for URL redirect and encoding it
-    if("x_af_r" in final_query):
+    if "x_af_r" in final_query:
         final_query["x_af_r"] = urllib.parse.quote(af_query["af_r"][0], safe="")
 
     # removing aaid from IOS links (if exists)
-    if ((os == "ios") and ("aaid" in final_query)):
+    if (os == "ios") and ("aaid" in final_query):
         del final_query["aaid"]
 
     # removing idfa from Android links (if exists)
-    if ((os == "android") and ("idfa" in final_query)):
+    if (os == "android") and ("idfa" in final_query):
         del final_query["idfa"]
 
     # last changes if trk link + standard redirect
-    if ((red == "yes") and (type == "trk")):
+    if (red == "yes") and (typeT == "trk"):
         # remove token if exists
-        if("token" in final_query):
+        if "token" in final_query:
             del final_query["token"]
 
     # last changes if trk link + S2S
-    #if ((red == "no") and (type == "trk")):
+    if (red == "no") and (typeT == "trk"):
+        paths2s = "s2s"
 
-    # last changes if imp link + standard Redirect
-    #if ((red == "yes") and (type == "imp")):
+        if "user_agent" not in final_query and "ip_address" not in final_query:
+            final_query["user_agent"] = "#USER_AGENT_MACRO#"
+            final_query["ip_address"] = "#IP_ADDRESS_MACRO#"
 
-    # last changes if imp link + S2S
-    #if ((red == "no") and (type == "imp")):
+        if "x_af_ip" in final_query and "x_af_ua" in final_query:
+            final_query["user_agent"] = final_query["x_af_ua"]
+            final_query["ip_address"] = final_query["x_af_ip"]
+            del final_query["x_af_ip"]
+            del final_query["x_af_ua"]
 
+        if "token" not in final_query:
+            if "token" in tg_query:
+                final_query["token"] = tg_query["token"][0]
+            else:
+                final_query["token"] = "#MISSING_TOKEN#"
+    else:
+        paths2s = tg_link.path
+
+        # last changes if imp link + standard Redirect
+        # if(typeT == "imp"):
 
     end_query = '&'.join('{}={}'.format(k, v) for k, v in final_query.items())
-    final_link = tg_link.scheme + "://" + tg_link.netloc + "/" + tg_link.path + "?" + tg_link.params + end_query
+    final_link = tg_link.scheme + "://" + tg_link.netloc + "/" + paths2s + "?" + tg_link.params + end_query
 
     return final_link
